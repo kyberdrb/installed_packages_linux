76e5c3aac94741462bbbda5b22981e23c950f1ee
kyberdrb <kyberdrb@gmail.com>
2022-12-31 12:02:44 +0100

    USB/IP: add client scripts to separate dir

--- usbip_resources/client/attach_printer.sh
+++ usbip_resources/client/attach_printer.sh
@@ -22,6 +22,13 @@ usbip port
 # TODO when the attaching of the printer fails, try to force-attach it
 #echo The password is ...? :)
 #ssh rpi@192.168.31.204 'systemctl reload usbip-printer.service'
+# Sources:
+#    - https://duckduckgo.com/?q=execute+one+command+ssh&ia=web
+#    - https://duckduckgo.com/?q=ssh+automatically+enter+password&ia=web
+#    - https://duckduckgo.com/?q=windows+ssh+login+without+password&ia=web
+#    - https://duckduckgo.com/?q=remotely+reload+systemd+service&ia=web
+#    - https://sleeplessbeastie.eu/2021/03/03/how-to-manage-systemd-services-remotely/
+
 #usbip list --remote=192.168.31.204
 #usbip attach --remote=192.168.31.204 --busid=1-1.3
 
